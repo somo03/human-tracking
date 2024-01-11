dbe76291c28144658bed8c15c703fd23ecf463e2
somo03 <sofia.molotkova@dfki.de>
2024-01-11 16:10:15 +0100

    added functionality to create new directory for save_path if it doesn't exist

--- helper.py
+++ helper.py
@@ -1,10 +1,25 @@
 import cv2
+import os
+import re
+
+from pathlib import Path
 
 
 def video_writer_same_codec(video: cv2.VideoCapture, save_path: str) -> cv2.VideoWriter:
     """
-    This function returns a VideoWriter object with the same codec as the input VideoCapture object
+    This function checks whether the directory of `save_path` exists and creates it if it's not.
+    If `save_path` is an empty string, the video will not be saved.
+
+    Returns: a VideoWriter object with the same codec as the input VideoCapture object.
     """
+
+    splitted = re.split('\\\|/', save_path)
+    dir_path = "/".join(splitted[:-1])
+
+    if save_path != "" and not os.path.exists(dir_path):
+        path = Path(dir_path)
+        path.mkdir(parents=True)
+
     w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(video.get(cv2.CAP_PROP_FPS))
