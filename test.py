8190f92d5a7cb6f57a130a3550d72004afd4a3cb
somo03 <sofia.molotkova@dfki.de>
2024-02-13 17:18:33 +0100

    changed test script location

--- test.py
+++ test.py
@@ -2,16 +2,16 @@ from human_tracking_api import TrackingAPI
 import os
 
 
-INPUT_VID = "C:\\Users\\somo03\\PythonProjects\\data\\video\\full\\B34_Xsens\\Moticon\\Runde 1\\Matte.mp4"
-#INPUT_VID = "C:\\Users\\somo03\\PythonProjects\\data\\video\\full\\B15_Xsens\\Moticon\\Runde 3\\matte"
+#INPUT_VID = "C:\\Users\\somo03\\PythonProjects\\data\\video\\full\\B34_Xsens\\Moticon\\Runde 1\\Matte.mp4"
+INPUT_VID = "C:\\Users\\somo03\\PythonProjects\\data\\video\\full\\B15_Xsens\\Moticon\\Runde 3\\matte\\Matte.mp4"
 print(os.getcwd())
 tracker = TrackingAPI(
-    1,
+    2,
     INPUT_VID,
-    "./data/output/B34_R4_Matte_test.mp4"
+    "./data/output/B15_R3_Matte_test.mp4"
 )
 tracker.track_people()
-#bboxes = tracker.get_bb_list()
+#bboxes = tracker.get_bb_liqst()
 
 '''
 with open(r'./tests/output.txt', 'w') as fp:
